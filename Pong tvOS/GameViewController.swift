e6597a16ae6d148ac28c7a5fefe86104be1c45e7
stevenquinn <stevenquinn@me.com>
2024-04-17 20:13:44 -0700

    Initial Commit

---
+++ Pong tvOS/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  Pong tvOS
+//
+//  Created by Steven Quinn on 4/17/24.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
